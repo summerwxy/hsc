4284b5b89c8712d55339eb251939552c97fa8147
0_o <summer.wxy@gmail.com>
2015-06-05 14:17:48 +0800

    update

--- 3-stat.py
+++ 3-stat.py
@@ -12,7 +12,7 @@ for row in ws.rows:
   source = row[14].value
   level = row[18].value
   if source in [u'經典', u'GVG']:
-    if not stat.has_key(source):
+    if not source in stat:
       stat[source] = {'total': 0, 'total_dust': 0, 'need': 0, 'need_dust': 0, 'lv0': 0, 'lv40': 0, 'lv100': 0, 'lv400': 0, 'lv1600': 0, 'tlv0': 0, 'tlv40': 0, 'tlv100': 0, 'tlv400': 0, 'tlv1600': 0}
     stat[source]['total'] += row[1].value
     stat[source]['total_dust'] += row[2].value
@@ -22,11 +22,11 @@ for row in ws.rows:
     stat[source]["t" + kv[level]] += row[1].value
 
 for it in [u'經典', u'GVG']:
-  print '===================================='
+  print('====================================')
   foo = stat[it]
-  print u'%s卡 缺: %s/%s(%s%%) 張 %s/%s(%s%%) 塵' % (it, foo['need'], foo['total'], foo['need'] * 100 / foo['total'], foo['need_dust'], foo['total_dust'], foo['need_dust'] * 100 / foo['total_dust'])
-  print u'最多 %s 包' % (foo['need_dust'] / 40)
-  print u'傳說 %s/%s 張 - 史詩 %s/%s 張 - 精良 %s/%s 張 - 普通 %s/%s 張' % (foo['lv1600'], foo['tlv1600'], foo['lv400'], foo['tlv400'], foo['lv100'], foo['tlv100'], foo['lv40'], foo['tlv40'])
+  print('%s卡 缺: %s/%s(%s%%) 張 %s/%s(%s%%) 塵' % (it, foo['need'], foo['total'], foo['need'] * 100 / foo['total'], foo['need_dust'], foo['total_dust'], foo['need_dust'] * 100 / foo['total_dust']))
+  print('最多 %s 包' % (foo['need_dust'] / 40))
+  print('傳說 %s/%s 張 - 史詩 %s/%s 張 - 精良 %s/%s 張 - 普通 %s/%s 張' % (foo['lv1600'], foo['tlv1600'], foo['lv400'], foo['tlv400'], foo['lv100'], foo['tlv100'], foo['lv40'], foo['tlv40']))
   
 
 
