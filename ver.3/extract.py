77996ada588fff044177e9f1ab0490d163c43aed
0_o <summer.wxy@gmail.com>
2015-10-09 15:56:57 +0800

    update

---
+++ ver.3/extract.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+hearthstonePath = "D:\misc\Hearthstone" # end without \
+
+# extract from hearthstone
+def extractFromHearthstone():
+  cmd = r"..\ver.2\disunity_v0.3.4\disunity.bat extract %s\Data\Win\cardxml0.unity3d" % (hearthstonePath)
+  os.system(cmd)
+
+
+if __name__ == '__main__':
+  extractFromHearthstone()
+
+
+
+
+
